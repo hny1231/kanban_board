f0a2586274ecf7421309ac552ce0a8bca429a08b
hny1231 <hny20210204@gmail.com>
2023-05-15 02:55:23 +0900

    hh add FileAdd Button test

--- frontend/src/pages/detail/CardDetail_origin.jsx
+++ frontend/src/pages/detail/CardDetail_origin.jsx
@@ -17,15 +17,13 @@ import { filledInputClasses } from "@mui/material";
 
 function CardDetail(props) {
     const cardId = props.cardId;
-    const open  = props.openDetailCard;
-    console.log("=============",props)
+    console.log(props);
+    const [openModal, setOpenModal] = useState(false);
     const { openDetailCard, setOpenDetailCard } = props;
     const serverIp = useSelector((state) => state.SERVER_IP);
     const admin = useSelector((state) => state.b_admin);
     const userId = useSelector((state) => state.u_id);
-    const fileUploadPath = useSelector((state) => state.FILE_UPLOAD_PATH);
 
-    const [selectedFiles, setSelectedFiles] = useState([]);
 
     // CardDetail 기본값 설정
     const [cardInfo, setCardInfo] = useState({
@@ -42,19 +40,21 @@ function CardDetail(props) {
         fileNameList: "",
     })
     function closeModal() {
+        // OpenDetailCard=false;
+        // openModal(false);
         setOpenDetailCard(false);
     }
 
 
     // 날짜, 카드파트너, 태그, 파일 리스트 조회
     // 더미 파일 주석
-    if(openDetailCard === true){
-        console.log(true);}
+ 
 
         async function getData() {
             try {
                 const response = await axios.get(serverIp + "detail/" + cardId);
                 const responseData = response.data;
+                console.log(responseData);
                 setCardInfo((prevState) => {
                     return {
                         ...prevState,
@@ -121,7 +121,6 @@ function CardDetail(props) {
         } catch (error) {
             console.log(error.message);
         }
-        saveFile(); 
         closeModal();
     }
     // 삭제 버튼 동작
@@ -141,64 +140,6 @@ function CardDetail(props) {
             console.log(error.message);
         }
     }
-
-    // 여기서부터
-    function handleFiles(e) {
-        setSelectedFiles([...selectedFiles, ...e.target.files]);
-      }
-    
-      function handleRemove(index) {
-        const files = [...selectedFiles];
-        files.splice(index, 1);
-        setSelectedFiles(files);
-      }
-    
-      function renderFileList() {
-        return selectedFiles.map((file, index) => (
-          <div key={index} className="file-item">
-            <span>{file.name}</span>
-            <A.Button type="button" onClick={() => handleRemove(index)}>
-              X
-            </A.Button>
-          </div>
-        ));
-      }
-    
-      function generateBoundary() {
-        let boundary = "--------------------------";
-        for (let i = 0; i < 24; i++) {
-          boundary += Math.floor(Math.random() * 10).toString(16);
-        }
-        return boundary;
-      }
-      function saveFile() {
-    
-        const formData = new FormData();
-    
-        formData.boundary = generateBoundary();
-        selectedFiles.forEach((file) => {
-          formData.append("file", file);
-          formData.append("c_id", 17);
-        });
-    
-        const headers = {
-          headers: {
-            'Content-Type': `multipart/form-data`
-    
-          }
-        };
-    
-        axios
-          .post(fileUploadPath, formData, headers)
-          .then((response) => {
-            console.log(response);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-        closeModal();
-      }
-    //   여기까지
     return (
 
         <div>
@@ -214,17 +155,6 @@ function CardDetail(props) {
                         <A.Input type="date" value={cardInfo.startDate} onChange={handleStartValueChange} />
                         <A.Input type="date" value={cardInfo.endDate} onChange={handleEndValueChange} />
                         <FileAdd fileLists/>
-                        <label htmlFor="file-upload" className="custom-file-upload">
-              <i className="fa fa-cloud-upload"></i> FileAdd
-            </label>
-            <input
-              id="file-upload"
-              className="fileButton"
-              type="file"
-              name="profile_files"
-              multiple
-              onChange={handleFiles}
-            />
                             <A.Div className="fileListTable">
                                 <A.Div className="cardWrap">
                                     <A.Div className="filebuttons"> File </A.Div></A.Div>
